d04c4d05f1eb5474e92b83382ee2f83de73221a5
Daniel Williams <danielpaulwill@gmail.com>
2022-02-20 22:32:53 -0500

    commit tree 6aaa195967d649d1236657245d4077c2c3699659

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,7 @@ require 'pry'
 # Write your code here
 
 def reverse_each_word(sentence)
-  sentence_arr = sentence.split
-  reversedArr = sentence_arr.map(&:reverse)
-  reversedArr.join(" ")
-  # sentence.split.map(&:reverse).join(" ")
+  sentence.split.map(&:reverse).join(" ")
 end
 
 binding.pry
